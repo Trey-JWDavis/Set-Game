ebadf6bb582e5429f826c263f5dc755675f2f619
Trey Davis <trey@jwdavis.tech>
2021-01-07 13:06:49 -0500

    Initial Commit

---
+++ Set Game/Set_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Set_GameApp.swift
+//  Set Game
+//
+//  Created by Trey Davis on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Set_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
